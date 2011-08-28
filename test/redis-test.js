671f5529db37bdfd319149616c7846aeb1cdd661
indexzero <charlie.robbins@gmail.com>
2011-08-28 10:39:33 -0400

    [fix test] Fix a couple of small issues in winston-redis.js and update tests accordingly

--- test/redis-test.js
+++ test/redis-test.js
@@ -9,6 +9,7 @@
 var path = require('path'),
     vows = require('vows'),
     assert = require('assert'),
+    redis = require('redis'),
     winston = require('winston'),
     helpers = require('winston/test/helpers'),
     Redis = require('../lib/winston-redis').Redis;
@@ -18,17 +19,42 @@ function assertRedis (transport) {
   assert.isFunction(transport.log);
 };
 
-var transport = new Redis();
+var options = { host: 'localhost', port: 6379, container: 'winston-test' },
+    transport = new Redis(options),
+    client = redis.createClient(options.port, options.host),
+    initial;
 
 vows.describe('winston-redis').addBatch({
  "An instance of the Redis Transport": {
+   topic: function () {
+     var that = this;
+     
+     client.llen(options.container, function (err, len) {
+       initial = len;
+       that.callback();
+     });
+   },
    "should have the proper methods defined": function () {
      assertRedis(transport);
    },
-   "the log() method": helpers.testNpmLevels(transport, "should log messages to redis", function (ign, err, meta, result) {
-     console.dir(arguments);
+   "the log() method": helpers.testNpmLevels(transport, "should log messages to redis", function (_, err, result) {
      assert.isTrue(!err);
-     assert.isObject(result);
+     assert.isTrue(result);
    })
  }
+}).addBatch({
+  "When the logging has completed": {
+    topic: function () {
+      var that = this;
+      
+      client.llen(options.container, function (err, len) {
+        return err 
+          ? that.callback(err) 
+          : setTimeout(that.callback.bind(that, null, len), 1000);
+       });
+    },
+    "the length should be incremented by the number of levels": function (err, updated) {
+      assert.isTrue(updated > initial);
+    }
+  }
 }).export(module);
